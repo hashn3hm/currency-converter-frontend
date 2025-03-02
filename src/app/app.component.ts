aca61c2442b1515d67679d0eb4771ddc98a73243
Muhammmad Al Hassan <hashn3hm@gmail.com>
2025-03-02 16:06:34 +0500

    update working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    CurrencyConverterComponent
+  ],
+  styleUrl: './app.component.scss',
+  template: `
+    <h1>Hello, currency-converter</h1>
+    <!-- The router will place the CurrencyConverterComponent here -->
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'currency-converter';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,15 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-// Angular Material Modules
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
-import { AppComponent } from './app.component';
-import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
+import { AppComponent } from '../app/app.component';
+import { CurrencyConverterComponent } from '../app/components/currency-converter/currency-converter.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        CurrencyConverterComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        FormsModule,
-        MatSelectModule,
-        MatFormFieldModule,
-        MatButtonModule,
-        MatProgressSpinnerModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    AppComponent,              // Import standalone component instead of declaring it
+    CurrencyConverterComponent // Import standalone component here as well
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
